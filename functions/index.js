d8bf0c8c747f9b0f8a9a3fcaa561e6a5813ee4e1
Nodax <test@test.com>
2020-11-16 22:09:15 +0900

    react対応

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const path = require('path');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+const app = express();
+
+//とりあえずスタティックの場所指定
+app.use(express.static(__dirname + '/react-app/src/'));
+
+//それ以外の時もindex.htmlへ
+app.all('*', (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, 'react-app/src/index.html'));
+})
+
+exports.app = functions.https.onRequest(app);
