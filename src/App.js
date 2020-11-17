a31fc5e7f93a4f7dc6a211794e6e9724abb4f83c
TsuyoshiNoda <noda-t@focus-s.com>
2020-11-17 16:57:00 +0900

    ページごとにjsファイル分割、コンポーネント対応

--- src/App.js
+++ src/App.js
@@ -1,93 +1,33 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import firebase from "./serviceAccount"
+import "firebase/firestore"
 
-const top = () => {
-  return (
-    <div>
-      <h1>kj-hilights</h1>
-      <p>これまでの生放送の瞬間最大コメント数を取得し、最もコメントが流れた2分間をまとめるページです</p>
+//各ページのコンポーネント群
+import top from "./top/top.js"
+import total from "./total/total.js"
+import weekly from "./weekly/weekly.js"
+import monthly from "./monthly/monthly.js"
+import threeMonthly from "./threeMonthly/threeMonthly.js"
+import sixMonthly from "./sixMonthly/sixMonthly.js"
 
-      <p>最新動画</p>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/cQLmPhdol4g" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+//コレクション取得
+const getCollection = () => {
+  const db = firebase.firestore();
+  //moviesコレクションを取得
+  const movies = db.collection("movies");
+  movies.get().then(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+      console.log(doc.id, " => ", doc.data());
+    });
+    console.log(querySnapshot.size);
+  })
+    .catch(function (error) {
+      console.log("Error getting documents: ", error);
+    });
+}
 
-      <Link to="/total">
-        <p>累計</p>
-      </Link>
-      <Link to="/weekly">
-        <p>直近1週間</p>
-      </Link>
-      <Link to="/monthly">
-        <p>直近1か月</p>
-      </Link>
-      <Link to="/3monthly">
-        <p>直近3か月</p>
-      </Link>
-      <Link to="/6monthly">
-        <p>直近半年</p>
-      </Link>
-    </div>
-  );
-};
-
-//累計total
-const total = () => {
-  return (
-    <div>
-      累計ランキング
-      <Link to="/">
-        <p>トップへ戻る</p>
-      </Link>
-    </div>
-  );
-};
-
-//週間weekly
-const weekly = () => {
-  return (
-    <div>
-      週間ランキング
-      <Link to="/">
-        <p>トップへ戻る</p>
-      </Link>
-    </div>
-  );
-};
-
-//月間monthly
-const monthly = () => {
-  return (
-    <div>
-      月間ランキング
-      <Link to="/">
-        <p>トップへ戻る</p>
-      </Link>
-    </div>
-  );
-};
-
-//3か月間3monthly
-const threeMonthly = () => {
-  return (
-    <div>
-      3か月間ランキング
-      <Link to="/">
-        <p>トップへ戻る</p>
-      </Link>
-    </div>
-  );
-};
-
-//6か月間6monthly
-const sixMonthly = () => {
-  return (
-    <div>
-      6か月間ランキング
-      <Link to="/">
-        <p>トップへ戻る</p>
-      </Link>
-    </div>
-  );
-};
+getCollection();
 
 const App = () => {
   return (
